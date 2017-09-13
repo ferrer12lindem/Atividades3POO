12684b3ad28540462c2d3ae101aa2ab86dd45dd6
Lindembergh Ferreira <30537403+ferrer12lindem@users.noreply.github.com>
2017-09-13 15:34:23 -0300

    Add files via upload

---
+++ Atividade3/src/questao6/Cliente.java
@@ -0,0 +1,35 @@
+package questao6;
+
+public class Cliente implements HomeTheaterFacade{
+
+	@Override
+	public void assistirFilme() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void finalizarFilmeEDesligar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ouvirCd() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ouvirRadio() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void desligarRadio() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Atividade3/src/questao6/HomeTheaterFacade.java
@@ -0,0 +1,10 @@
+package questao6;
+
+public interface HomeTheaterFacade {
+
+	public void assistirFilme();
+	public void finalizarFilmeEDesligar();
+	public void ouvirCd();
+	public void ouvirRadio();
+	public void desligarRadio();
+}
